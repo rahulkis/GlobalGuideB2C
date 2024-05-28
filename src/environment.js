2a0ab2851af3f1ab41a376145b51067a3cb124b6
Lenovo <rahulb.kis@gmail.com>
2024-05-28 19:21:06 +0530

    env file

--- src/environment.js
+++ src/environment.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = "http://3.93.66.218:8000/api/";
-export const IMAGE_BASE_URL = "http://3.93.66.218:8000/images/";
+export const API_BASE_URL = "http://35.153.234.62:8001/api/";
+export const IMAGE_BASE_URL = "http://35.153.234.62:8001/images/";
