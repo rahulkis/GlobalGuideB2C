494173e05620b6c68640fcc5d4d5101f899a579f
Lenovo <rahulb.kis@gmail.com>
2024-05-28 19:20:25 +0530

    url changes

--- src/constants/Api.js
+++ src/constants/Api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-export const API_BASE_URL = "http://3.93.66.218:8000/api/";
-export  const IMAGE_BASE_URL = 'http://3.93.66.218:8000/images/';
+export const API_BASE_URL = "http://35.153.234.62:8001/api/";
+export  const IMAGE_BASE_URL = 'http://35.153.234.62:8001/images/';
 
 export const httpClient = axios.create({
   baseUrl: API_BASE_URL,
